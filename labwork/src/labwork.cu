3200d8e73e477d9f08a79c8774cc5a2f3b57a40f
tranthuykieu <thuykieu2423@gmail.com>
2020-11-02 20:21:01 +0700

    labwork 1 - OpenMP

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -38,6 +38,7 @@ int main(int argc, char **argv) {
             printf("labwork 1 CPU ellapsed %.1fms\n", lwNum, timer.getElapsedTimeInMilliSec());
             timer.start();
             labwork.labwork1_OpenMP();
+            printf("labwork 1 CPU openmp ellapsed %.1fms\n", lwNum, timer.getElapsedTimeInMilliSec());
             labwork.saveOutputImage("labwork2-openmp-out.jpg");
             break;
         case 2:
@@ -110,6 +111,16 @@ void Labwork::labwork1_OpenMP() {
     int pixelCount = inputImage->width * inputImage->height;
     outputImage = static_cast<char *>(malloc(pixelCount * 3));
     // do something here
+    
+    #pragma omp parallel for
+    for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
+        for (int i = 0; i < pixelCount; i++) {
+            outputImage[i * 3] = (char) (((int) inputImage->buffer[i * 3] + (int) inputImage->buffer[i * 3 + 1] +
+                                          (int) inputImage->buffer[i * 3 + 2]) / 3);
+            outputImage[i * 3 + 1] = outputImage[i * 3];
+            outputImage[i * 3 + 2] = outputImage[i * 3];
+        }
+    }
 }
 
 int getSPcores(cudaDeviceProp devProp) {
@@ -190,7 +201,6 @@ void Labwork::labwork9_GPU() {
 
 void Labwork::labwork10_GPU(){
 }
-
 
 
 
